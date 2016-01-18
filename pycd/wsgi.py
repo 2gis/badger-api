6d03b7e820c7475dbd500de58e250fd865e06897
ira <ira@debian>
2016-01-18 10:00:33 +0600

    fix flake8

--- pycd/wsgi.py
+++ pycd/wsgi.py
@@ -6,11 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pycd.settings")
+
+from django.core.wsgi import get_wsgi_application  # noqa
+from whitenoise.django import DjangoWhiteNoise  # noqa
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
